c5774e05d1c075d70fffc4c014c55b565d4e2bdb
suhani <1008suhaniparmar@gmail.com>
2025-02-12 17:17:21 +0530

    add controllers and repositories

---
+++ Unifyx_Backend/src/main/java/org/example/unifyx/Repository/ContractorProfileRepository.java
@@ -0,0 +1,10 @@
+package org.example.unifyx.Repository;
+
+import org.example.unifyx.Model.ContractorProfile;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ContractorProfileRepository extends JpaRepository<ContractorProfile, Integer> {
+    // You can add custom queries here if necessary
+}
