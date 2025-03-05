62d7034e424cc4fbe58cc5e5f6726e03e027a7b6
Parmar Suhani <124229698+Suhaniparmar@users.noreply.github.com>
2025-03-05 13:23:33 +0530

    Delete Unifyx_Backend/Unifyx_Backend directory

--- Unifyx_Backend/Unifyx_Backend/src/main/java/org/example/unifyx/controller/WorkerHome.java
+++
@@ -1,9 +0,0 @@
-package org.example.unifyx.controller;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-@Controller
-@RequestMapping("/worker/home")
-public class WorkerHome {
-}
--- Unifyx_Backend/Unifyx_Backend/src/main/java/org/example/unifyx/service/HomepageService.java
+++
@@ -1,7 +0,0 @@
-package org.example.unifyx.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class HomepageService {
-}
